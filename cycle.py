1c5dadb2c2ddaa2e6e2b8f9beefb782252954383
AphilK <joaosilvanascimento.jops@gmail.com>
2022-08-09 00:51:17 -0300

    Adicionei comentários no cycle.py

--- cycle.py
+++ cycle.py
@@ -1,10 +1,11 @@
+#A ideia aqui é fazer um programa que calcula sequências cíclicas
 print("Opa, tudo bom meu caro ?\nVamos fazer umas sequências cíclicas ?\nSimbora então!")
 word = input("Primeiro me diga qual será a palavra de hoje:\n")
-arr = list(word)
-num = len(arr)
+arr = list(word) #Cria um array com a palavra
+num = len(arr) #Aqui é a quantidade de letras
 
 quantity = int(input("Qual a posição da letra desejada ?\n"))
-
+#Aqui pegamos o resto da divisão entre a posição e a quantidade de letras, dando a posição da letra
 position = quantity % num
 letter = arr[position]
 
