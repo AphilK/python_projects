e90e809aa2056b2e1bb17096d57f22abb3d2f620
AphilK <joaosilvanascimento.jops@gmail.com>
2022-08-09 01:48:50 -0300

    Atualizando

--- Adivinhe o numero/guess.py
+++ Adivinhe o numero/guess.py
@@ -5,10 +5,11 @@ x = int(random() * 10) #Numero aleatorio
 print("Vamos jogar um jogo, voce consegue adivinhar o numero que estou pensando ?\nDigite o numero que voce acha que estou pensando:")
 y = int(input())
 
-while y > x:
-    y = int(input("Diminua!\n"))
-while y < x:
-    y = int(input("Aumente!\n"))
-if y == x :
-    print("Parabens! você acertou!")
+while y != x:
+    if y < x:
+        y = int(input("Tente novamente, muito baixo\n"))
+    elif y > x:
+        y = int(input("Tente novamente, muito acima\n"))
+    
+print("Parabens! voce acertou")
 quit()
